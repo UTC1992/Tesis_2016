21cf35572263c1be59cd4376ed304b418d2b5107
UTC1992 <omar.guanoluisa25@gmail.com>
2016-04-30 13:06:54 -0500

    fecha-30-04-2016

---
+++ BusinessIntelligen_UTC/BusinessIntelligen_UTC/Content/UI_Fabri/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" href="./css/fabric.min.css">
+ 	<link rel="stylesheet" href="./css/fabric.components.min.css">
+
+	<title></title>
+</head>
+<body>
+	<h1>Hola mundo</h1>
+	<i class="ms-Icon ms-Icon--group" style="font-size:xx-large;" aria-hidden="true">Personas</i>
+	<br>
+	<button class="ms-Button" id="get-data-from-selection">
+	<span class="ms-Button-icon">
+	<i class="ms-Icon ms-Icon--plus"></i></span>
+	<span class="ms-Button-label">Get Data from selection</span>
+	<span class="ms-Button-description">Get Data from the document selection</span>
+	</button>
+</body>
+</html>
